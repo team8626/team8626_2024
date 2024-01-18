66b6577d0dd44ba722df7f7591e2732bba32dc5f
jackr876 <102693981+jackr876@users.noreply.github.com>
2024-01-18 14:34:17 -0500

    created shooter

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+/** Add your docs here. */
+public class ShooterConstants {
+    public static final int kCANMotor1 = 0;
+    public static final int kCANMotor2 = 0;
+    public static final int kIRSensor = 0; //DIO
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Dashboard.DashboardUses;
+import frc.robot.subsystems.Dashboard.ImplementDashboard;
+
+public class ShooterSubsystem extends SubsystemBase implements ImplementDashboard {
+  private static CANSparkMax m_motor1 = new CANSparkMax(ShooterConstants.kCANMotor1, MotorType.kBrushless);
+  private static CANSparkMax m_motor2 = new CANSparkMax(ShooterConstants.kCANMotor2, MotorType.kBrushless);
+  private static DigitalInput m_infrared = new DigitalInput(ShooterConstants.kIRSensor);
+  /** Creates a new IntakeSubsystem. */
+  public ShooterSubsystem() {
+    m_motor2.setInverted(true);
+  }
+
+  public static void setMotors(double speed){
+    m_motor1.set(speed);
+    m_motor2.set(speed);
+  }
+  public static boolean getSensor(){
+    // 0 means something is there, 1 means nothing is there
+    return m_infrared.get();
+  }
+  @Override
+  public void periodic(){}
+
+  @Override
+  public void initDashboard() {
+  }
+
+  @Override
+  public void updateDashboard() {
+  }
+
+  @Override
+  public DashboardUses getDashboardUses() {
+    return DashboardUses.SHORT_INTERVAL;
+  }
+}
