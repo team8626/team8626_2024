b66abf194254d1b727b6a1c5875d02617b99d990
nedf123 <nedfinkle@student.scit.org>
2024-01-18 13:16:24 -0500

    Victor

--- src/main/java/frc/robot/subsystems/kitbotshooter/KitBotShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/kitbotshooter/KitBotShooterSubsystem.java
@@ -7,16 +7,19 @@ package frc.robot.subsystems.kitbotshooter;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.climber.ClimberConstants;
 
+
 public class KitBotShooterSubsystem extends SubsystemBase {
 
-  private static CANSparkMax m_motorLeft = new CANSparkMax(ClimberConstants.kClimberMotorLeftPort, MotorType.kBrushless);
-  private static CANSparkMax m_motorRight = new CANSparkMax(ClimberConstants.kClimberMotorRightPort, MotorType.kBrushless);
+  private static VictorSP m_motorLeft = new VictorSP(7);
+ private static VictorSP m_motorRight = new VictorSP(23);
+
 
   public KitBotShooterSubsystem() {
-    m_motorRight.setInverted(true);
+    m_motorRight.setInverted(false);
   }
 
 public void setMotors(double output) {
