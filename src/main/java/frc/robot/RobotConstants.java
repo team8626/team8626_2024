e0308da5f1403b346c1bcebed9f0c919eebcc2ba
git_master <team8626.sw@gmail.com>
2024-02-04 18:55:28 -0500

    Create Skeleton for Arm Control Code

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -18,4 +18,16 @@ public class RobotConstants {
 
   public static Translation2d intakeOffset =
       new Translation2d(Units.inchesToMeters(24), Units.inchesToMeters(0));
+
+  public static double kFrameWidth = Units.inchesToMeters(24);
+  public static double kFrameLength = Units.inchesToMeters(24);
+
+  public static double kArmOffsetXMeters = Units.inchesToMeters(-6.50);
+  public static double kArmOffsetYMeters = Units.inchesToMeters(0);
+  public static double kArmOffsetZMeters = Units.inchesToMeters(16.5);
+  public static double kArmRotationRollOffsetDeg = 0;
+  public static double kArmRotationPitchOffsetDeg = -90;
+  public static double kArmRotationYawOffsetDeg = 0;
+  public static double kArmExtensionOffMeters = Units.inchesToMeters(-3);
+  ;
 }
