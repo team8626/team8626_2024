f40dc4d9d811ca9dccb56102bbe9f318edf83f92
Chris <chris@nautilytics.com>
2024-02-06 17:29:54 -0500

    Revert "Revert "Merge branch 'main' into ArmExtensionSubsytem""
    This reverts commit 2554d43b9bbb663da3e5b9335bddab50d2c91257.

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Dashboard.DashboardUses;
 import frc.robot.subsystems.Dashboard.ImplementDashboard;
+import frc.utils.Vision;
 import java.io.File;
 import java.util.function.DoubleSupplier;
 import swervelib.SwerveController;
@@ -43,9 +44,8 @@ public class SwerveSubsystem extends SubsystemBase implements ImplementDashboard
   private final SwerveDrive swerveDrive;
   /** Maximum speed of the robot in meters per second, used to limit acceleration. */
   public double maximumSpeed = Units.feetToMeters(14.5);
-  /** Vision object */
-  private Vision m_vision = new Vision();
-  /** Publisher for robot pose (AdvantageScope) */
+
+  /** Publisher for robot pose (AdvantageScxope) */
   StructPublisher<Pose3d> m_publisher =
       NetworkTableInstance.getDefault().getStructTopic("Robot Pose", Pose3d.struct).publish();
 
@@ -302,7 +302,20 @@ public class SwerveSubsystem extends SubsystemBase implements ImplementDashboard
   }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+
+    // Correct pose estimate with vision measurements
+    var visionEst = m_vision.getEstimatedGlobalPose();
+    visionEst.ifPresent(
+        est -> {
+          var estPose = est.estimatedPose.toPose2d();
+          // Change our trust in the measurement based on the tags we can see
+          var estStdDevs = m_vision.getEstimationStdDevs(estPose);
+
+          swerveDrive.addVisionMeasurement(
+              est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
+        });
+  }
 
   @Override
   public void simulationPeriodic() {}
