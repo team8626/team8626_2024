30ba62d88eee7c6d67ccf3d625fa33c636c7f6b7
ajs0907 <aidanjsheehan07@gmail.com>
2024-02-06 16:16:59 -0500

    L

--- src/main/java/frc/robot/subsystems/LEDs/LEDConstants.java
+++ src/main/java/frc/robot/subsystems/LEDs/LEDConstants.java
@@ -1,12 +1,24 @@
 package frc.robot.subsystems.LEDs;
 
-import java.lang.reflect.Array;
+public class LEDConstants {
+  public static final int kLEDLength = 30;
+  public static final int kLEDPort = 0;
+  public static final int[] kErrorLightIndex = {0, 1, 2, 3, 4, 25, 26, 27, 28, 29};
 
-import edu.wpi.first.wpilibj.DigitalOutput;
-
-public class LEDConstants { 
-    public static final int kLEDLength = 30;
-    public static final int kLEDPort = 0;
-    public static final int[] kErrorLight = {0,1,2,3,4,25,26,27,28,29};
-
+  public static final int minLoopCycleCount = 10;
+  public static final int length = 10;
+  public static final double strobeFastDuration = 0.1;
+  public static final double strobeSlowDuration = 0.2;
+  public static final double breathDuration = 1.0;
+  public static final double rainbowCycleLength = 25.0;
+  public static final double rainbowDuration = 0.25;
+  public static final double waveExponent = 0.4;
+  public static final double waveFastCycleLength = 25.0;
+  public static final double waveFastDuration = 0.25;
+  public static final double waveSlowCycleLength = 25.0;
+  public static final double waveSlowDuration = 3.0;
+  public static final double waveAllianceCycleLength = 15.0;
+  public static final double waveAllianceDuration = 2.0;
+  public static final double autoFadeTime = 2.5; // 3s nominal
+  public static final double autoFadeMaxTime = 5.0;
 }
--- src/main/java/frc/robot/subsystems/LEDs/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDs/LEDSubsystem.java
@@ -6,40 +6,126 @@ package frc.robot.subsystems.LEDs;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
-  private Timer timer = new Timer();
-  private static AddressableLED m_AddressableLED = new AddressableLED(LEDConstants.kLEDLength);
-  
-  public LEDSubsystem(){
-    
+  private static AddressableLED m_LEDs;
+
+  private AddressableLEDBuffer m_buffer;
+
+  public enum LedMode {
+    NOT_CONNECTED,
+    CONNECTED,
+    ESTOPPED,
+    AUTO_FINISHED,
+    AUTONOMOUS,
+    HANGING,
+    SHOOTING,
+    CONTAINING,
+    INTAKING,
+    BLUE_ALLIANCE,
+    RED_ALLIANCE,
+    LOW_BATTERY_ALERT,
+    DISABLED,
+    OFF,
   }
 
-  public void setColor(Color color){
-     int kLEDBufferLength = 30;
-     AddressableLEDBuffer buffer = new AddressableLEDBuffer(kLEDBufferLength);
+  LedMode mode = LedMode.OFF;
+
+  public void setMode(LedMode m_mode) {
+    mode = m_mode;
+  }
+
+  public LEDSubsystem() {
+
+    m_LEDs = new AddressableLED(LEDConstants.kLEDPort);
+    m_buffer = new AddressableLEDBuffer(LEDConstants.kLEDLength);
+    m_LEDs.setLength(LEDConstants.kLEDLength);
+    m_LEDs.setData(m_buffer);
+    m_LEDs.start();
+    // this.setColor(Color.kRed);
+
+  }
+
+  public void setColor(Color color) {
+    int kLEDBufferLength = 30;
+    // AddressableLEDBuffer buffer = new AddressableLEDBuffer(kLEDBufferLength);
     for (int i = 0; i < LEDConstants.kLEDLength; i++) {
-      buffer.setLED(i, color);
+      m_buffer.setLED(i, color);
     }
-    m_AddressableLED.setData(buffer);
-
+    m_LEDs.setData(m_buffer);
   }
-    public void getColor(){
-      
+
+  private void updateLeds() {
+    switch (mode) {
+      case SHOOTING:
+        this.wave(Color.kOrange, Color.kYellow, 7.0, 2.0);
+        break;
+      case BLUE_ALLIANCE:
+        this.wave(Color.kBlue, Color.kNavy, 7.0, 2.0);
+        break;
+
+      default:
+        this.wave(Color.kBlack, Color.kWhite, 7.0, 2.0);
+        break;
     }
+  }
+
+  private void wave(Color c1, Color c2, double cycleLength, double duration) {
+    double x = (1 - ((Timer.getFPGATimestamp() % duration) / duration)) * 2.0 * Math.PI;
+    double xDiffPerLed = (2.0 * Math.PI) / cycleLength;
+    for (int i = 0; i < LEDConstants.kLEDLength; i++) {
+      x += xDiffPerLed;
+      // if (i >= LEDConstants.kLEDLength){
+      double ratio = (Math.pow(Math.sin(x), LEDConstants.waveExponent) + 1.0) / 2.0;
+      if (Double.isNaN(ratio)) {
+        ratio = (-Math.pow(Math.sin(x + Math.PI), LEDConstants.waveExponent) + 1.0) / 2.0;
+      }
+      if (Double.isNaN(ratio)) {
+        ratio = 0.5;
+      }
+      double red = (c1.red * (1 - ratio)) + (c2.red * ratio);
+      double green = (c1.green * (1 - ratio)) + (c2.green * ratio);
+      double blue = (c1.blue * (1 - ratio)) + (c2.blue * ratio);
+      m_buffer.setLED(i, new Color(red, green, blue));
+      m_LEDs.setData(m_buffer);
+      // }
+    }
+  }
 
   @Override
- 
-  
-    public void periodic() {
+  public void periodic() {
+
+    updateLeds();
+
     // This method will be called once per scheduler run
 
-    
+    // if (DriverStation.isDSAttached()) {
+    // if (DriverStation.getAlliance().get() == Alliance.Blue) {
+    // this.wave(Color.kAquamarine, Color.kAzure, 25.0, 2.0);
+    // }
+    // }
+
+    // Check DS Status
+    // -> Not Attached => Fushia/Pink wave(P1, P2, x, y)
+    // -> Attached => Alliance Color Blue? B1, B2... Red? R1, R2...
+
   }
 }
+
+// Optional<Alliance> alliance = DriverStation.getAlliance();
+//     if (alliance.isPresent()) {
+//       if (alliance.get() == Alliance.Blue) {
+//         this.wave(Color.kBlue, Color.kNavy, 7.0, 2.0);
+//       }
+//       if (alliance.get() == Alliance.Red) {
+//         this.wave(Color.kRed, Color.kMaroon, 7.0, 2.0);
+//       }
+//     } else {
+//       this.wave(Color.kPink, Color.kWhite, 7.0, 2.0);
+//     }
+//     }
+//   }
