45052361aee3f09edc6d94130a60914b521e17a8
git_master <team8626.sw@gmail.com>
2024-02-15 16:04:37 -0500

    fix: ShooterCommand Prototype in auto commands

--- src/main/java/frc/robot/commands/ShootFromAmpCommand.java
+++ src/main/java/frc/robot/commands/ShootFromAmpCommand.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.commands.SetArmCommand;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.shooter.ShooterConstants;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.shooter.commands.ShooterCommand;
@@ -17,9 +18,9 @@ import frc.robot.subsystems.shooter.commands.ShooterCommand;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ShootFromAmpCommand extends SequentialCommandGroup {
 
-  public ShootFromAmpCommand(ShooterSubsystem shooter, ArmSubsystem arm) {
+  public ShootFromAmpCommand(IntakeSubsystem intake, ShooterSubsystem shooter, ArmSubsystem arm) {
     addCommands(
         new SetArmCommand(arm, ArmConstants.Presets.kShootAmplifier_0ft),
-        new ShooterCommand(shooter, ShooterConstants.kAmplifierShootSpeed));
+        new ShooterCommand(intake, shooter, ShooterConstants.kShootFromSpeakerRPM));
   }
 }
