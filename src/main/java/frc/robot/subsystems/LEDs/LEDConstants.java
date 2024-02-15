cd3c24573e89166ffe9f29ae4d9a788c75483506
git_master <team8626.sw@gmail.com>
2024-02-15 15:35:41 -0500

    Pre Debugging

---
+++ src/main/java/frc/robot/subsystems/LEDs/LEDConstants.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.LEDs;
+
+public class LEDConstants {
+  public static final int kLEDLength = 75;
+  public static final int kLEDPort = 0;
+  public static final int[][] kErrorLightIndex = {
+    {0, 1, 2, 3, 4, 5}, {20, 21, 22, 23, 24}, {45, 46, 47, 48, 49}, {70, 71, 72, 73, 74}
+  };
+
+  public static final int minLoopCycleCount = 10;
+  public static final int length = 10;
+  public static final double strobeFastDuration = 0.1;
+  public static final double strobeSlowDuration = 0.2;
+  public static final double breathDuration = 1.0;
+  public static final double rainbowCycleLength = 25.0;
+  public static final double rainbowDuration = 0.25;
+  public static final double waveExponent = 0.4;
+  public static final double waveFastCycleLength = 25.0;
+  public static final double waveFastDuration = 0.25;
+  public static final double waveSlowCycleLength = 25.0;
+  public static final double waveSlowDuration = 3.0;
+  public static final double waveAllianceCycleLength = 15.0;
+  public static final double waveAllianceDuration = 2.0;
+  public static final double autoFadeTime = 2.5; // 3s nominal
+  public static final double autoFadeMaxTime = 5.0;
+}
