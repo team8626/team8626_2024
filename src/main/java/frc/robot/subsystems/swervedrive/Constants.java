e7fa6c86f9c6def62595de682534d7cb81871148
nedf123 <nedfinkle@student.scit.org>
2024-02-16 15:19:54 -0500

    Remove Old Drive Library

--- src/main/java/frc/robot/subsystems/swervedrive/Constants.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Constants.java
@@ -45,5 +45,8 @@ public final class Constants {
     public static final double LEFT_Y_DEADBAND = 0.1;
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT = 6;
+    public static final int kXboxControllerPort = 0;
+    public static final int kTestControllerPort = 0;
+    public static final int kButtonBoxPort = 0;
   }
 }
