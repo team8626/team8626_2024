491b5c191eca8d6353959e57fd1b05e037607bcf
git_master <team8626.sw@gmail.com>
2024-02-17 18:25:44 -0500

    Update Constants

--- src/main/java/frc/robot/SubsystemsConstants.java
+++ src/main/java/frc/robot/SubsystemsConstants.java
@@ -2,14 +2,14 @@ package frc.robot;
 
 public class SubsystemsConstants {
   public static enum Preset {
-    kStart(110, 0, 500, 500),
-    kStow(195, 0, 500, 500),
-    kFloorPickup(20, 5, 500, 500),
+    kStart(110, 0, 0, 0),
+    kStow(195, 0, 0, 0),
+    kFloorPickup(200, 10, 500, 500),
     kShootSpeaker_0m(195, 0, 4000, 5500),
-    kShootSpeaker_2m(20, 0, 500, 500),
-    kShootSpeaker_3m(20, 0, 500, 500),
+    kShootSpeaker_2m(185, 0, 500, 500),
+    kShootSpeaker_3m(187, 0, 500, 500),
     kShootAmp(80, 6, 1000, 1000),
-    kStartClimb(90, 10, 500, 500);
+    kStartClimb(90, 10, 0, 0);
 
     private final double m_rot;
     private final double m_ext;
