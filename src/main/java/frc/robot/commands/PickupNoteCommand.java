7895d524833e6060650f66dc0f71e773fde56295
git_master <team8626.sw@gmail.com>
2024-02-19 05:52:11 -0500

    Moved Presets to Presets.java

--- src/main/java/frc/robot/commands/PickupNoteCommand.java
+++ src/main/java/frc/robot/commands/PickupNoteCommand.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.SubsystemsConstants.Preset;
+import frc.robot.Presets.Preset;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.commands.SetArmCommand;
 import frc.robot.subsystems.intake.IntakeSubsystem;
