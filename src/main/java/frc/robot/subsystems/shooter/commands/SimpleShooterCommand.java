e6a41f162fd7c18b7fa6677b7274218603307c55
git_master <team8626.sw@gmail.com>
2024-02-19 05:44:35 -0500

    Moved Presets to Presets.java

--- src/main/java/frc/robot/subsystems/shooter/commands/SimpleShooterCommand.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/SimpleShooterCommand.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems.shooter.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.SubsystemsConstants.Preset;
+import frc.robot.Presets.Preset;
 import frc.robot.subsystems.LEDs.LEDConstants.LedMode;
 import frc.robot.subsystems.LEDs.LEDSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
