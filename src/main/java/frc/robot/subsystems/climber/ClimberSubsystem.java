ae0a827add9593a6abc077184bfa509bfef309e3
git_master <team8626.sw@gmail.com>
2024-02-28 13:11:41 -0500

    Add Climber Subsystem

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.climber;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Dashboard.DashboardUses;
+import frc.robot.subsystems.Dashboard.ImplementDashboard;
+
+public class ClimberSubsystem extends SubsystemBase implements ImplementDashboard {
+
+  /** Create Motors * */
+  private final VictorSPX m_motorLeft;
+
+  private final VictorSPX m_motorRight;
+
+  private boolean m_enabled = false;
+
+  /** Creates a new ArmExtensionSubsystem. */
+  public ClimberSubsystem() {
+    // Reset & Initialize Controllers
+    m_motorLeft = new VictorSPX(ClimberConstants.CANID_L);
+    m_motorRight = new VictorSPX(ClimberConstants.CANID_R);
+
+    m_motorLeft.setInverted(false);
+    m_motorRight.setInverted(true);
+
+    m_motorRight.follow(m_motorLeft);
+  }
+
+  /**
+   * Set Motors Power
+   *
+   * @newPower new value to be applied [-1.0 ; 1.0]
+   */
+  public void setPower(double newPower) {
+    if (newPower != 0) {
+      m_enabled = true;
+    } else {
+      m_enabled = false;
+    }
+    m_motorLeft.set(VictorSPXControlMode.PercentOutput, newPower);
+  }
+
+  @Override
+  public void initDashboard() {}
+
+  @Override
+  public void updateDashboard() {
+    SmartDashboard.putBoolean("Climber/ENABLED", m_enabled);
+  }
+
+  @Override
+  public DashboardUses getDashboardUses() {
+    return DashboardUses.SHORT_INTERVAL;
+  }
+}
