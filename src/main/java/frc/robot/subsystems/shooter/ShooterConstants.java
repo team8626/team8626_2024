57ef791211fc971071aeff0bd437ee8072f4a08f
git_master <team8626.sw@gmail.com>
2024-02-29 09:05:29 -0500

    Shooter Tuning

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -10,7 +10,23 @@ public class ShooterConstants {
   public static final int kCANMotor2 = 25;
   public static final int kMaxRPM = 5700;
 
-  public static final int kRPMTolerance = 100;
+  public static final double kP_top = 0.00005;
+  public static final double kI_top = 0.0000004;
+  public static final double kD_top = 0.0;
+  public static final double kIz_top = 5000;
+  public static final double kFF_top = 0.0;
+  public static final double kMaxOutput_top = 1;
+  public static final double kMinOutput_top = -1;
+
+  public static final double kP_bottom = 0.00005;
+  public static final double kI_bottom = 0.0000004;
+  public static final double kD_bottom = 0.0;
+  public static final double kIz_bottom = 5000;
+  public static final double kFF_bottom = 0;
+  public static final double kMaxOutput_bottom = 1;
+  public static final double kMinOutput_bottom = -1;
+
+  public static final int kRPMTolerance = 1;
 
   public class ShooterStates {
     public static enum ShooterStatus {
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -11,6 +11,7 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.Dashboard.DashboardUses;
@@ -19,13 +20,13 @@ import frc.robot.subsystems.preset.Presets.Preset;
 import frc.robot.subsystems.shooter.ShooterConstants.ShooterStates.ShooterStatus;
 
 public class ShooterSubsystem extends SubsystemBase implements ImplementDashboard {
-  private CANSparkMax m_motor1;
-  private CANSparkMax m_motor2;
-  private SparkPIDController m_pidController1;
-  private SparkPIDController m_pidController2;
+  private CANSparkMax m_motor_Bottom;
+  private CANSparkMax m_motor_Top;
+  private SparkPIDController m_pidController_Bottom;
+  private SparkPIDController m_pidController_Top;
 
-  private RelativeEncoder m_encoder1;
-  private RelativeEncoder m_encoder2;
+  private RelativeEncoder m_encoder_Bottom;
+  private RelativeEncoder m_encoder_Top;
 
   private double m_desiredRPM_Bottom = Preset.kShootSpeaker_0m.getBottomRPM();
   private double m_desiredRPM_Top = Preset.kShootSpeaker_0m.getTopRPM();
@@ -36,54 +37,85 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
   private boolean m_isAtSpeed = false;
   private ShooterStatus m_status = ShooterStatus.IDLE;
 
-  private double m_kP = 0.00015;
-  private double m_kI = 0;
-  private double m_kD = 0.00038;
-  private double m_kIz = 0;
-  private double m_kFF = 0.00017;
-  private double m_kMaxOutput = 1;
-  private double m_kMinOutput = -1;
+  private double m_kP_top;
+  private double m_kI_top;
+  private double m_kD_top;
+  private double m_kIz_top;
+  private double m_kFF_top;
+  private double m_kMaxOutput_top;
+  private double m_kMinOutput_top;
+
+  private double m_kP_bottom;
+  private double m_kI_bottom;
+  private double m_kD_bottom;
+  private double m_kIz_bottom;
+  private double m_kFF_bottom;
+  private double m_kMaxOutput_bottom;
+  private double m_kMinOutput_bottom;
 
   /** Creates a new IntakeSubsystem. */
   public ShooterSubsystem() {
-    m_motor1 = new CANSparkMax(ShooterConstants.kCANMotor1, MotorType.kBrushless);
-    m_motor1.restoreFactoryDefaults();
-    m_motor1.setIdleMode(IdleMode.kCoast);
-    m_motor1.setInverted(true);
 
-    m_motor2 = new CANSparkMax(ShooterConstants.kCANMotor2, MotorType.kBrushless);
-    m_motor2.restoreFactoryDefaults();
-    m_motor2.setIdleMode(IdleMode.kCoast);
-    m_motor2.setInverted(true);
+    m_kP_top = ShooterConstants.kP_top;
+    m_kI_top = ShooterConstants.kI_top;
+    m_kD_top = ShooterConstants.kD_top;
+    m_kIz_top = ShooterConstants.kIz_top;
+    m_kFF_top = ShooterConstants.kFF_top;
+    m_kMaxOutput_top = ShooterConstants.kMaxOutput_top;
+    m_kMinOutput_top = ShooterConstants.kMinOutput_top;
 
-    m_pidController1 = m_motor1.getPIDController();
-    m_pidController2 = m_motor2.getPIDController();
+    m_kP_bottom = ShooterConstants.kP_bottom;
+    m_kI_bottom = ShooterConstants.kI_bottom;
+    m_kD_bottom = ShooterConstants.kD_bottom;
+    m_kIz_bottom = ShooterConstants.kIz_bottom;
+    m_kFF_bottom = ShooterConstants.kFF_bottom;
+    m_kMaxOutput_bottom = ShooterConstants.kMaxOutput_bottom;
+    m_kMinOutput_bottom = ShooterConstants.kMinOutput_bottom;
 
-    m_encoder1 = m_motor1.getEncoder();
-    m_encoder2 = m_motor2.getEncoder();
+    m_motor_Bottom = new CANSparkMax(ShooterConstants.kCANMotor1, MotorType.kBrushless);
+    m_motor_Bottom.restoreFactoryDefaults();
+    m_motor_Bottom.setIdleMode(IdleMode.kCoast);
+    m_motor_Bottom.setInverted(true);
+
+    m_motor_Top = new CANSparkMax(ShooterConstants.kCANMotor2, MotorType.kBrushless);
+    m_motor_Top.restoreFactoryDefaults();
+    m_motor_Top.setIdleMode(IdleMode.kCoast);
+    m_motor_Top.setInverted(true);
+
+    m_pidController_Bottom = m_motor_Bottom.getPIDController();
+    m_pidController_Top = m_motor_Top.getPIDController();
+
+    m_encoder_Bottom = m_motor_Bottom.getEncoder();
+    m_encoder_Top = m_motor_Top.getEncoder();
 
     // set PID coefficients
-    m_pidController1.setP(m_kP);
-    m_pidController1.setI(m_kI);
-    m_pidController1.setD(m_kD);
-    m_pidController1.setIZone(m_kIz);
-    m_pidController1.setFF(m_kFF);
-    m_pidController1.setOutputRange(m_kMinOutput, m_kMaxOutput);
+    m_pidController_Bottom.setP(m_kP_bottom);
+    m_pidController_Bottom.setI(m_kI_bottom);
+    m_pidController_Bottom.setD(m_kD_bottom);
+    m_pidController_Bottom.setIZone(m_kIz_bottom);
+    m_pidController_Bottom.setFF(m_kFF_bottom);
+    m_pidController_Bottom.setOutputRange(m_kMinOutput_bottom, m_kMaxOutput_bottom);
 
-    m_pidController2.setP(m_kP);
-    m_pidController2.setI(m_kI);
-    m_pidController2.setD(m_kD);
-    m_pidController2.setIZone(m_kIz);
-    m_pidController2.setFF(m_kFF);
-    m_pidController2.setOutputRange(m_kMinOutput, m_kMaxOutput);
+    m_pidController_Top.setP(m_kP_top);
+    m_pidController_Top.setI(m_kI_top);
+    m_pidController_Top.setD(m_kD_top);
+    m_pidController_Top.setIZone(m_kIz_top);
+    m_pidController_Top.setFF(m_kFF_top);
+    m_pidController_Top.setOutputRange(m_kMinOutput_top, m_kMaxOutput_top);
+
+    // SmartDashboard.putNumber("Shooter/PGainBottom", 0);
+    // SmartDashboard.putNumber("Shooter/IGainBottom", 0);
+    // SmartDashboard.putNumber("Shooter/DGainBottom", 0);
+    // SmartDashboard.putNumber("Shooter/FFBottom", 0);
   }
 
-  public void setMotors(double speed) {
-    m_pidController1.setReference(speed, CANSparkMax.ControlType.kVelocity);
-    m_pidController2.setReference(speed, CANSparkMax.ControlType.kVelocity);
-    // m_motor1.set(speed);
-    // m_motor2.set(speed);
-  }
+  // TODO: Remove
+  // public void setBottomPIDs(double p, double i, double d, double iZone) {
+  // m_pidController_Bottom.setP(p);
+  // m_pidController_Bottom.setI(i);
+  // m_pidController_Bottom.setD(d);
+  // m_pidController_Bottom.setIZone(iZone);
+  // }
 
   public void setRPM(double speed_Bottom, double speed_Top) {
     m_desiredRPM_Bottom = speed_Bottom;
@@ -99,6 +131,11 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
 
     // Force Dashboard Update
     initDashboard();
+  }
+
+  public void start(Preset newPreset) {
+    this.setRPM(newPreset.getBottomRPM(), newPreset.getTopRPM());
+    this.start();
   }
 
   public void start() {
@@ -131,24 +168,20 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
 
   @Override
   public void periodic() {
-
     // Clamp Values
     m_desiredRPM_Bottom = MathUtil.clamp(m_desiredRPM_Bottom, 0, ShooterConstants.kMaxRPM);
     m_desiredRPM_Top = MathUtil.clamp(m_desiredRPM_Top, 0, ShooterConstants.kMaxRPM);
 
     if (Robot.isReal()) {
       if (m_enabled) {
-        // m_motor1.set(0.9);
-        // m_motor2.set(0.9);
-
-        m_pidController1.setReference(m_desiredRPM_Bottom, CANSparkMax.ControlType.kVelocity);
-        m_pidController2.setReference(m_desiredRPM_Top, CANSparkMax.ControlType.kVelocity);
+        m_pidController_Bottom.setReference(m_desiredRPM_Bottom, CANSparkMax.ControlType.kVelocity);
+        m_pidController_Top.setReference(m_desiredRPM_Top, CANSparkMax.ControlType.kVelocity);
       } else {
-        m_pidController1.setReference(0, CANSparkMax.ControlType.kDutyCycle);
-        m_pidController2.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+        m_pidController_Bottom.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+        m_pidController_Top.setReference(0, CANSparkMax.ControlType.kDutyCycle);
       }
-      m_currentRPM_Bottom = m_encoder1.getVelocity();
-      m_currentRPM_Top = m_encoder2.getVelocity();
+      m_currentRPM_Bottom = m_encoder_Bottom.getVelocity();
+      m_currentRPM_Top = m_encoder_Top.getVelocity();
 
     } else if (Robot.isSimulation()) {
       if (m_enabled) {
@@ -177,7 +210,16 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
   }
 
   @Override
-  public void initDashboard() {}
+  public void initDashboard() {
+    SmartDashboard.putNumber("Shooter/P Gain Top", m_kP_top);
+    SmartDashboard.putNumber("Shooter/I Gain Top ", m_kI_top);
+    SmartDashboard.putNumber("Shooter/D Gain Top", m_kD_top);
+    SmartDashboard.putNumber("Shooter/Feed Forward Top", m_kFF_top);
+    SmartDashboard.putNumber("Shooter/P Gain Bottom", m_kP_bottom);
+    SmartDashboard.putNumber("Shooter/I Gain Bottom ", m_kI_bottom);
+    SmartDashboard.putNumber("Shooter/D Gain Bottom", m_kD_bottom);
+    SmartDashboard.putNumber("Shooter/Feed Forward Bottom", m_kFF_bottom);
+  }
 
   @Override
   public void updateDashboard() {
@@ -192,30 +234,57 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
       m_desiredRPM_Top = newRPM_Top;
     }
 
-    double p = SmartDashboard.getNumber("Shooter/P Gain", m_kP);
-    double d = SmartDashboard.getNumber("Shooter/D Gain", m_kD);
-    double ff = SmartDashboard.getNumber("Shooter/Feed Forward", m_kFF);
+    double p_top = SmartDashboard.getNumber("Shooter/P Gain Top", m_kP_top);
+    double i_top = SmartDashboard.getNumber("Shooter/I Gain Top", m_kI_top);
+    double d_top = SmartDashboard.getNumber("Shooter/D Gain Top", m_kD_top);
+    double ff_top = SmartDashboard.getNumber("Shooter/Feed Forward Top", m_kFF_top);
+    double p_bottom = SmartDashboard.getNumber("Shooter/P Gain Bottom", m_kP_bottom);
+    double i_bottom = SmartDashboard.getNumber("Shooter/I Gain Bottom", m_kI_bottom);
+    double d_bottom = SmartDashboard.getNumber("Shooter/D Gain Bottom", m_kD_bottom);
+    double ff_bottom = SmartDashboard.getNumber("Shooter/Feed Forward Bottom", m_kFF_bottom);
 
     // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if ((p != m_kP)) {
-      m_pidController1.setP(p);
-      m_pidController2.setP(p);
-      m_kP = p;
+    if ((p_top != m_kP_top)) {
+      System.out.printf("[SHOOTER] Updating Top P to %f, was %f\n", m_kP_top, p_top);
+
+      m_pidController_Top.setP(p_top);
+      m_kP_top = p_top;
     }
-    if ((d != m_kD)) {
-      m_pidController1.setD(d);
-      m_pidController2.setD(d);
-      m_kD = d;
+    if ((i_top != m_kI_top)) {
+      System.out.printf("[SHOOTER] Updating Top I to %f, was %f\n", m_kI_top, i_top);
+      m_pidController_Top.setI(i_top);
+      m_kI_top = i_top;
     }
-    if ((ff != m_kFF)) {
-      m_pidController1.setFF(ff);
-      m_pidController2.setFF(ff);
-      m_kFF = ff;
+    if ((d_top != m_kD_top)) {
+      m_pidController_Top.setD(d_top);
+      m_kD_top = d_top;
+    }
+    if ((ff_top != m_kFF_top)) {
+      m_pidController_Top.setFF(ff_top);
+      m_kFF_top = ff_top;
+    }
+
+    if ((p_bottom != m_kP_bottom)) {
+      m_pidController_Bottom.setP(p_bottom);
+      m_kP_bottom = p_bottom;
+    }
+    if ((i_bottom != m_kI_bottom)) {
+      m_pidController_Bottom.setI(i_bottom);
+      m_kI_bottom = i_bottom;
+    }
+
+    if ((d_bottom != m_kD_bottom)) {
+      m_pidController_Bottom.setD(d_bottom);
+      m_kD_bottom = d_bottom;
+    }
+
+    if ((ff_bottom != m_kFF_bottom)) {
+      m_pidController_Bottom.setFF(ff_bottom);
+      m_kFF_bottom = ff_bottom;
     }
 
     SmartDashboard.putString("Shooter/Status", m_status.getString());
     SmartDashboard.putBoolean("Shooter/ENABLED", m_enabled);
-    m_enabled = SmartDashboard.getBoolean("Shooter/ENABLED", m_enabled);
     SmartDashboard.putBoolean("Shooter/AtSpeed", isAtSpeed());
 
     SmartDashboard.putNumber("Shooter/DesiredRPM_Bottom", m_desiredRPM_Bottom);
@@ -223,14 +292,15 @@ public class ShooterSubsystem extends SubsystemBase implements ImplementDashboar
 
     SmartDashboard.putNumber("Shooter/CurrentRPM_Bottom", m_currentRPM_Bottom);
     SmartDashboard.putNumber("Shooter/CurrentRPM_Top", m_currentRPM_Top);
-
-    SmartDashboard.putNumber("Shooter/P Gain", m_kP);
-    SmartDashboard.putNumber("Shooter/D Gain", m_kD);
-    SmartDashboard.putNumber("Shooter/Feed Forward", m_kFF);
   }
 
   @Override
   public DashboardUses getDashboardUses() {
     return DashboardUses.SHORT_INTERVAL;
+  }
+
+  public Command startCmd() {
+    return startEnd(() -> this.m_enabled = true, () -> this.m_enabled = false)
+        .withName("[SHOOTER] StartCmd " + m_desiredRPM_Top + "/" + m_desiredRPM_Bottom);
   }
 }
