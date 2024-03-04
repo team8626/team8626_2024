97ee0d771eacfda79bb7b152766528b8f6106fa3
git_master <team8626.sw@gmail.com>
2024-03-04 18:53:55 -0500

    Add Pose2d to Presets

--- src/main/java/frc/robot/subsystems/preset/Presets.java
+++ src/main/java/frc/robot/subsystems/preset/Presets.java
@@ -1,30 +1,57 @@
 package frc.robot.subsystems.preset;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Presets {
   public enum Preset {
-    kStart("START", 180, 0, 0, 0),
-    kStow("STOW", 200, 0, 0, 0),
-    kFloorPickup("FLOOR PICKUP", 203, 11, 500, 500),
-    kShootInsidePerimeter("SUBWOOFER INSIDE", 201, 0, 2200, 4700),
+    kStart("START", 180, 0),
+    kStow("STOW", 200, 0),
+    kFloorPickup("FLOOR PICKUP", 203, 11),
+    kStartClimb("START_CLIMB", 90, 10),
+
     kShootSpeaker_0m("SPEAKER_0M", 198, 10, 4000, 5200),
     kShootSpeaker_2m("SPEAKER_2M", 187, 0, 4500, 4500),
     kShootSpeaker_3m("SPEAKER_3M", 187, 0, 4000, 4000),
     kShootAmp("AMP", 80, 10, 3000, 3000),
     kLongPass("PASS", 170, 0, 5500, 5500),
-    kDiscardNote("DISCARD", 185, 0, 0, 5500),
-    kStartClimb("START_CLIMB", 90, 10, 0, 0);
+    kShootInsidePerimeter("SUBWOOFER INSIDE", 201, 0, 2200, 4700),
+
+    kShootPodium(
+        "PODIUM BLUE", 180, 0, 4000, 5200, new Pose2d(2.6, 4.3, Rotation2d.fromDegrees(23.3)));
 
     private double m_rot;
     private double m_ext;
     private int m_topRPM;
     private int m_bottomRPM;
+    private Pose2d m_robotPose;
     private String m_string;
+
+    Preset(String Name, double Rot_Deg, double Ext_In) {
+      this.m_rot = Rot_Deg;
+      this.m_ext = Ext_In;
+      this.m_topRPM = 0;
+      this.m_bottomRPM = 0;
+      this.m_robotPose = null;
+      this.m_string = Name;
+    }
 
     Preset(String Name, double Rot_Deg, double Ext_In, int TopRPM, int BottomRPM) {
       this.m_rot = Rot_Deg;
       this.m_ext = Ext_In;
       this.m_topRPM = TopRPM;
       this.m_bottomRPM = BottomRPM;
+      this.m_robotPose = null;
+      this.m_string = Name;
+    }
+
+    Preset(
+        String Name, double Rot_Deg, double Ext_In, int TopRPM, int BottomRPM, Pose2d robotPose) {
+      this.m_rot = Rot_Deg;
+      this.m_ext = Ext_In;
+      this.m_topRPM = TopRPM;
+      this.m_bottomRPM = BottomRPM;
+      this.m_robotPose = robotPose;
       this.m_string = Name;
     }
 
@@ -42,6 +69,10 @@ public class Presets {
 
     public int getBottomRPM() {
       return m_bottomRPM;
+    }
+
+    public Pose2d getPose() {
+      return m_robotPose;
     }
 
     public String getString() {
