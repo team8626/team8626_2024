faf66a8abbd35c0a57f9da32960f866a12968e65
git_master <team8626.sw@gmail.com>
2024-03-09 08:40:48 -0500

    Spotless

--- src/main/java/frc/utils/Vision.java
+++ src/main/java/frc/utils/Vision.java
@@ -34,7 +34,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -201,8 +200,7 @@ public class Vision implements ImplementDashboard {
   public void initDashboard() {}
 
   @Override
-  public void updateDashboard() {
-  }
+  public void updateDashboard() {}
 
   @Override
   public DashboardUses getDashboardUses() {
