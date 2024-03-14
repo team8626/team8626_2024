16ea42e25c3530fd56b79279308fd35c28d59585
git_master <team8626.sw@gmail.com>
2024-03-14 12:26:44 -0400

    Actually Shoot

--- src/main/java/frc/robot/commands/auto/AimAndShootCommand.java
+++ src/main/java/frc/robot/commands/auto/AimAndShootCommand.java
@@ -6,21 +6,40 @@ package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.subsystems.arm.SetArmCommand;
 import frc.robot.commands.subsystems.drive.TurnToAngleCommand;
+import frc.robot.commands.subsystems.shooter.SpinAndShootCommand;
 import frc.robot.subsystems.LEDs.LEDConstants.LedMode;
 import frc.robot.subsystems.LEDs.LEDSubsystem;
+import frc.robot.subsystems.arm.extension.ArmExtensionSubsystem;
+import frc.robot.subsystems.arm.rotation.ArmRotationSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.preset.PresetManager;
+import frc.robot.subsystems.preset.Presets;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class AimAndShootCommand extends SequentialCommandGroup {
 
-  public AimAndShootCommand(SwerveSubsystem drive) { // }, Supplier<Preset> desiredState) {
+  public AimAndShootCommand(
+      SwerveSubsystem drive,
+      IntakeSubsystem intake,
+      ShooterSubsystem shooter,
+      ArmRotationSubsystem armRot,
+      ArmExtensionSubsystem armExt) { // }, Supplier<Preset> desiredState) {
     setName("AimAndShootCommand");
 
     addCommands(
         new InstantCommand(() -> LEDSubsystem.setMode(LedMode.AUTOSHOOT)),
         new TurnToAngleCommand(
             drive, () -> PresetManager.getAimAndShootPreset(drive.getPose()).getPose(), true),
+        new SpinAndShootCommand(
+            intake,
+            shooter,
+            armRot,
+            armExt,
+            () -> PresetManager.getAimAndShootPreset(drive.getPose())),
+        new SetArmCommand(armRot, armExt, () -> Presets.kStow),
         new InstantCommand(() -> LEDSubsystem.setMode(LedMode.DEFAULT)));
   }
 }
