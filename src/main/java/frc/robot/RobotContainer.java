d9b8831e451f6a120d88f7aad1fc8c7198826ee3
clementdumet <76540126+clementdumet@users.noreply.github.com>
2024-06-01 07:30:57 -0400

    Fix LED Colors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -466,7 +466,7 @@ public class RobotContainer {
         .toggleOnTrue(
             new AimAndDriveCommand(
                     m_drivebase, m_intake, m_shooter, m_armRot, m_armExt, m_xboxController, this)
-                    .alongWith(new InstantCommand(() -> m_presetStorage.set(Presets.kShootSubwoofer)))
+                .alongWith(new InstantCommand(() -> m_presetStorage.set(Presets.kShootSubwoofer)))
                 .handleInterrupt(
                     () ->
                         new SetArmCommand(m_armRot, m_armExt, () -> Presets.kStow)
@@ -483,7 +483,7 @@ public class RobotContainer {
         .button_5()
         .toggleOnTrue(
             new EasyAmpCommand(m_drivebase, m_xboxController, this)
-            .alongWith(new InstantCommand(() -> m_presetStorage.set(Presets.kShootAmp)))
+                .alongWith(new InstantCommand(() -> m_presetStorage.set(Presets.kShootAmp)))
                 .andThen(
                     new SetArmCommand(m_armRot, m_armExt, () -> Presets.kShootAmp)
                         .andThen(new ShootAmpCommand(m_intake, m_shooter)))
